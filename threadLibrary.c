fc7b12adb41219335ef12646da387269051da475
Kartikeya Joshi <91309849+kartikeyajoshi3009@users.noreply.github.com>
2023-11-15 02:55:05 +0530

    Update threadLibrary.c
    Added show, resume, run and suspend
    Remaining delete at line 25 and sleep at line 58.

--- threadLibrary.c
+++ threadLibrary.c
@@ -23,8 +23,57 @@ int createThread(threadLib* tL, void (*function)(void*), void* arg){
     }
 }
 void deleteThread(threadLib* tL, int threadId){}
-void suspendThread(threadLib* tL, int threadId){}
-void resumeThread(threadLib* tL, int threadId){}
-void runThread(threadLib* tL, int threadId){}
+void suspendThread(threadLib* tL, int threadId){
+    for(int i=0; i<tL->threadCount; i++){
+        if(tL->threads[i].id==threadId){
+            pthread_mutex_lock(&(tL->threads[i].mutex));
+            tL->threads[i].status=SUSPENDED;
+            pthread_mutex_unlock(&(tL->threads[i].mutex));
+            break;
+        }
+    }
+}
+void resumeThread(threadLib* tL, int threadId){
+    for(int i=0; i<tL->threadCount; i++){
+        if(tL->threads[i].id==threadId){
+            pthread_mutex_lock(&(tL->threads[i].mutex));
+            tL->threads[i].status=READY;
+            pthread_cond_signal(&(tL->threads[i].condition));
+            pthread_mutex_unlock(&(tL->threads[i].mutex));
+            break;
+        }
+    }
+}
+void runThread(threadLib* tL, int threadId){
+    for(int i=0; i<tL->threadCount; i++){
+        if(tL->threads[i].id==threadId){
+            pthread_mutex_lock(&(tL->threads[i].mutex));
+            tL->threads[i].status=RUNNING;
+            pthread_create(&(tL->threads[i].thread), NULL, tL->threads[i].function, tL->threads[i].arg);
+            pthread_mutex_unlock(&(tL->threads[i].mutex));
+            break;
+        }
+    }
+}
 void sleepThread(threadLib* tL, int threadId, int t){}
-void showThreadStatus(threadLib* tL, int threadId){}
+void showThreadStatus(threadLib* tL, int threadId){
+    for(int i=0; i<tL->threadCount; i++){
+        if(tL->threads[i].id==threadId){
+            printf("Thread %d Status: ", tL->threads[i].id);
+            switch (tL->threads[i].status)
+            {
+            case READY:
+                printf("READY\n");
+                break;
+            case RUNNING:
+                printf("RUNNING\n");
+            case SUSPENDED:
+                printf("SUSPENDED\n");
+            default:
+                printf("TERMINATED\n");
+                break;
+            }
+            break;
+        }
+    }
+}
