cee4957f69a15eb746804552919c404ef8bbef3c
Kartikeya Joshi <91309849+kartikeyajoshi3009@users.noreply.github.com>
2023-11-15 02:32:32 +0530

    Update threadLibrary.h
    Added threadLib struct definition.
    Added declearation to create, delete, suspend, resume, run, sleep and display status functions.

--- threadLibrary.h
+++ threadLibrary.h
@@ -1,7 +1,9 @@
 #ifndef THREADLIBRARY_H
 #define THREADLIBRARY_H
 #include<pthread.h>
+
 typedef enum threadStatus{READY, RUNNING, SUSPENDED, TERMINATED}threadStatus;
+
 typedef struct threadControlBlock{
     pthread_t thread;
     int id;
@@ -11,4 +13,19 @@ typedef struct threadControlBlock{
     pthread_mutex_t mutex;
     pthread_cond_t condition;
 }threadControlBlock;
+
+typedef struct threadLib
+{
+    threadControlBlock* threads;
+    int threadCount;
+    int nextThreadId;
+}threadLib;
+
+int createThread(threadLib* tL, void(*function)(void*), void* arg);
+void deleteThread(threadLib* tL, int threadId);
+void suspendThread(threadLib* tL, int threadId);
+void resumeThread(threadLib *tL, int threadId);
+void runThread(threadLib *tL, int threadId);
+void sleepThread(threadLib *tL, int threadId, int t);
+void showThreadStatus(threadLib* tL, int threadId);
 #endif
